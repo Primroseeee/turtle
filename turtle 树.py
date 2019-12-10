e37796aca5a47bb5531d05dc685202b637c925cd
Primroseeee <56987380+Primroseeee@users.noreply.github.com>
2019-12-10 22:28:51 +0800

    Update turtle 树.py
    增加了注释

--- turtle 树.py
+++ turtle 树.py
@@ -7,49 +7,45 @@
 import turtle as t
 def draw_branch(length):
     if length >= 10:
+        #画右侧树枝
         t.forward(length)
         t.right(30)
         draw_branch(length - 10)
-
-        t.left(60)
+        
+        #画左侧树枝
+        t.left(60)  
         draw_branch(length - 10)
 
+        #返回之前节点
         t.right(30)
         if length <=10:
-            t.pencolor("yellow")
-            star()
+            t.pencolor("yellow") #画笔颜色为黄色
+            star()               #画五角星
         else:
-            t.pencolor("red")
-            t.circle(5)
+            t.pencolor("red")  #画笔颜色为红色
+            t.circle(5)        #画圆
         t.backward(length)
 
-def star():
-    t.fillcolor("yellow")
+def star():               #画五角星
+    t.fillcolor("yellow") #填充黄色
     t.begin_fill()
     j = 1
     while j <= 5:   
-        t.forward(20)   
-        t.left(144)    
+        t.forward(20)   #向前20
+        t.left(144)     #逆时针转144度
         j = j + 1
     t.end_fill()
 
 def main():
-    t.penup()
-    t.backward(100)
-    t.pendown()
-    t.pensize(2)
-    t.left(90)
-    draw_branch(80)
-    t.exitonclick()
+    t.penup()        #抬起画笔
+    t.backward(100)  #后退100
+    t.pendown()      #落下画笔
+    t.pensize(2)     #画笔宽度2
+    t.left(90)       #逆时针转90度，画笔朝上
+    draw_branch(80)  #长度为80
+    t.exitonclick()  #d点击后退出turtle
     
 
     
 if __name__ == "__main__" :
     main()
-
-
-# In[ ]:
-
-
-
-
